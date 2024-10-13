648fc73ea096e168aee2e0477aca4e569def0ac7
Nahae <148427441+NahyeChoi@users.noreply.github.com>
2024-10-13 20:53:22 +0900

    [Gold IV] Title: 좋다, Time: 152 ms, Memory: 14656 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,42 +1,67 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
- 
+
+//1253
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(arr);
-		int ans = 0;
-		
-		for(int i=0; i<N; i++) { 
-			int left = 0; 
-			int right = N-1; 
-			while(true) { 
-				
-				if (i == left) left++;
-				else if (right == i) right--;
-				
-				if (left >= right)	break;
-				
-				if (arr[left] + arr[right] > arr[i]) right--;
-				else if (arr[left] + arr[right] < arr[i]) left++;
-				else {
-					ans++;
-					break;
-				}
-			}
-		}
-		System.out.println(ans);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+
+        int GOOD = 0;
+        for (int i = 0; i < N; i++){
+            int start = 0;
+            int end = N-1;
+
+//            반례 : 자신을 포함할 때는 제외
+//            while (start < end) {
+//                int sum = A[start] + A[end];
+//                if (sum > A[i]) {
+//                    end--;
+//                } else if (sum < A[i]) {
+//                    start++;
+//                } else {
+//                    if (start == i) {
+//                        start++;
+//                        continue;
+//                    }else if (end == i) {
+//                        end--;
+//                        continue;
+//                    }else {
+//                        GOOD++;
+//                    }
+//                    break;
+//                }
+//            }
+
+            while (true) {
+                if (start == i) { start++; }
+                else if (end == i) { end--; }
+
+                if (start >= end) { break; }
+                
+                int sum = A[start] + A[end];
+                if (sum > A[i]) { end--; }
+                else if (sum < A[i]) { start++; }
+                else {
+                    GOOD++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(GOOD);
+    }
 }

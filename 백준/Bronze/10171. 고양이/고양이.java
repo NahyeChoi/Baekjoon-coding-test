1cb031695d53b939c1af3068a7d37ee7a769f6b6
Nahae <148427441+NahyeChoi@users.noreply.github.com>
2024-11-29 13:08:48 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 14028 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+//10171
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
